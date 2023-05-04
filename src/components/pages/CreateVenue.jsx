49fd26fc80b4c62e20fe997a9c9e7c4d9dc9f231
hreinn <hreinngy@gmail.com>
2023-05-04 11:12:52 +0200

    Adds Create form styling with validation

--- src/components/pages/CreateVenue.jsx
+++ src/components/pages/CreateVenue.jsx
@@ -1,5 +1,160 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup
+    .string()
+    .trim()
+    .required('Venue name is a required field')
+    .max(90, 'Venue name can not exceed 90 characters'),
+
+  description: yup
+    .string()
+    .trim()
+    .required('Description is a required field')
+    .max(450, 'Description can not exceed 450 characters'),
+
+  price: yup
+    .number()
+    .typeError('Price must be a number')
+    .min(1, 'Price must be at least 1')
+    .max(50000, 'Price can not exceed 50.000 kr NOK'),
+
+  maxGuests: yup
+    .number()
+    .typeError('Max guests must be a number')
+    .min(1, 'Max guests must be at least 1')
+    .max(200, 'Max guests can not exceed 200'),
+
+  media: yup
+    .string()
+    .trim()
+    .required('Image URL is a required field')
+    .max(450, 'Description can not exceed 450 characters')
+    .matches(/\.(jpg|jpeg|png|webp|avif|gif|svg)$/, 'Image URL is not valid'),
+});
+
 function CreateVenue() {
-  return <div>Create Venue</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <main className={'mt-[120px] min-h-[50vh] sm:mt-12'}>
+        <section id={'profile'} className={'mt-[88px] mb-12 sm:mt-12 md:mb-28'}>
+          <div className={'container mx-auto px-4 max-w-7xl'}>
+            <form
+              autoComplete={'off'}
+              onSubmit={handleSubmit(onSubmit)}
+              className={
+                'rounded-xl px-6 pt-10 pb-8 border border-gray-100 shadow-sm shadow-gray-100 md:max-w-[568px] md:my-0 md:mx-auto'
+              }
+            >
+              <h1 className={'text-4xl font-bold text-center mb-10'}>Create Venue</h1>
+              <div className={'flex flex-col gap-4'}>
+                <label className={'w-full'}>
+                  <input
+                    {...register('name')}
+                    className={`border-gray-200 border rounded h-10 indent-4 w-full ${errors.name && 'border-red-700'}`}
+                    type={'text'}
+                    placeholder={'Name of venue'}
+                  />
+                </label>
+                {errors.name && <p className={'text-red-700'}>{errors.name?.message}</p>}
+                <label className={'w-full'}>
+                  <textarea
+                    {...register('description')}
+                    className={`border-gray-200 border rounded h-40 indent-4 pt-2 w-full ${
+                      errors.description && 'border-red-700'
+                    }`}
+                    placeholder={'Description'}
+                  />
+                </label>
+                {errors.description && <p className={'text-red-700'}>{errors.description?.message}</p>}
+                <label className={'w-full'}>
+                  <input
+                    {...register('price')}
+                    className={`border-gray-200 border rounded h-10 indent-4 w-full ${
+                      errors.price && 'border-red-700'
+                    }`}
+                    type={'number'}
+                    placeholder={'Price'}
+                  />
+                </label>
+                {errors.price && <p className={'text-red-700'}>{errors.price?.message}</p>}
+                <label className={'w-full'}>
+                  <input
+                    {...register('maxGuests')}
+                    className={`border-gray-200 border rounded h-10 indent-4 w-full ${
+                      errors.maxGuests && 'border-red-700'
+                    }`}
+                    type={'number'}
+                    placeholder={'Max guests'}
+                  />
+                </label>
+                {errors.maxGuests && <p className={'text-red-700'}>{errors.maxGuests?.message}</p>}
+                <label className={'w-full'}>
+                  <input
+                    {...register('media')}
+                    className={`border-gray-200 border rounded h-10 indent-4 w-full ${
+                      errors.media && 'border-red-700'
+                    }`}
+                    type={'text'}
+                    placeholder={'Image URL'}
+                  />
+                </label>
+                {errors.media && <p className={'text-red-700'}>{errors.media?.message}</p>}
+                <div id={'meta-checkboxes'} className={'text-sm flex gap-4 my-4 mx-auto md:ml-4'}>
+                  <div className={'flex items-center gap-1.5'}>
+                    <label htmlFor={'wifi'} className={'font-semibold select-none'}>
+                      Wifi
+                    </label>
+                    <input id={'wifi'} type={'checkbox'} />
+                  </div>
+                  <div className={'flex items-center gap-1.5'}>
+                    <label htmlFor={'parking'} className={'font-semibold select-none'}>
+                      Parking
+                    </label>
+                    <input id={'parking'} type={'checkbox'} />
+                  </div>
+                  <div className={'flex items-center gap-1.5'}>
+                    <label htmlFor={'breakfast'} className={'font-semibold select-none'}>
+                      Breakfast
+                    </label>
+                    <input id={'breakfast'} type={'checkbox'} />
+                  </div>
+                  <div className={'flex items-center gap-1.5'}>
+                    <label htmlFor={'pets'} className={'font-semibold select-none'}>
+                      Pets
+                    </label>
+                    <input id={'pets'} type={'checkbox'} />
+                  </div>
+                </div>
+                <button
+                  type={'submit'}
+                  className={
+                    'relative font-semibold rounded bg-rose-800 text-white h-10 w-full hover:bg-rose-700 ease-out duration-200'
+                  }
+                >
+                  Create Venue
+                  {/* {isLoading && <span className={'loader absolute top-2 left-6 h-6 w-6'}></span>}
+                  {isLoading ? 'Processing..' : 'Create Venue'}*/}
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default CreateVenue;
