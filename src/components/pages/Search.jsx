b272c6b6012e9d4f496f6d6636a5d42cf03ae875
hreinn <hreinngy@gmail.com>
2023-08-24 20:16:34 +0200

    Fixes search page jumping on back button

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -25,33 +25,28 @@ function Search() {
         <section id={'venues'} className={'mt-[88px] mb-12 sm:mt-12'}>
           <div className={'container mx-auto px-4 max-w-7xl'}>
             <h1 className={'text-4xl font-bold mb-10'}>Search Results</h1>
-            {isLoading ? (
+            {isLoading && (
               <>
-                <div className={'my-0 mx-auto w-fit min-h-[50vh]'}>
+                <div className={'absolute left-0 right-0 min-h-screen flex justify-center bg-white'}>
                   <div className={'loader'}></div>
                 </div>
               </>
-            ) : (
-              <>
-                {!isError && (
-                  <h2 className={'text-lg font-normal mb-6'}>
-                    {searchResults.length > 0 ? 'Results for ' : 'No matches for '}{' '}
-                    <span className={'italic font-bold'}>{`'${value.trim()}'`}</span>
-                  </h2>
-                )}
-                <div
-                  id={'venues-container'}
-                  className={
-                    'flex flex-col gap-14 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4'
-                  }
-                >
-                  {searchResults.length > 0 &&
-                    filterVenuesWithProperties(searchResults).map((venue) => (
-                      <VenueListItem key={venue.id} {...venue} />
-                    ))}
-                </div>
-              </>
             )}
+            {!isError && (
+              <h2 className={'text-lg font-normal mb-6'}>
+                {searchResults.length > 0 ? 'Results for ' : 'No matches for '}{' '}
+                <span className={'italic font-bold'}>{`'${value.trim()}'`}</span>
+              </h2>
+            )}
+            <div
+              id={'venues-container'}
+              className={
+                'flex flex-col gap-14 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4'
+              }
+            >
+              {searchResults.length > 0 &&
+                filterVenuesWithProperties(searchResults).map((venue) => <VenueListItem key={venue.id} {...venue} />)}
+            </div>
             {isError && (
               <div className={'api-error sm:w-fit'}>
                 <p>Something went wrong..</p>
